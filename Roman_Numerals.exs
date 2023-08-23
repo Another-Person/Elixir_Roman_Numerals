d38b5f30b0aafa95591c5fc376e624c2197c7b33
Joey Sachtleben <manjoey7@gmail.com>
2023-08-22 21:04:17 -0500

    Expanded number range

--- Roman_Numerals.exs
+++ Roman_Numerals.exs
@@ -7,6 +7,14 @@ defmodule RomanNumerals do
     stringSoFar
   end
 
+  defp intToRomanHelper(num, stringSoFar) when num >= 10 do
+    intToRomanHelper(num - 10, "#{stringSoFar}X")
+  end
+
+  defp intToRomanHelper(num, stringSoFar) when num == 9 do
+    intToRomanHelper(num - 9, "#{stringSoFar}IX")
+  end
+
   defp intToRomanHelper(num, stringSoFar) when num >= 5 do
     intToRomanHelper(num - 5, "#{stringSoFar}V")
   end
@@ -24,9 +32,6 @@ end
 one = RomanNumerals.intToRoman(1)
 IO.puts("Expected I got #{one}")
 
-three = RomanNumerals.intToRoman(3)
-IO.puts("Expected III got #{three}")
-
 four = RomanNumerals.intToRoman(4)
 IO.puts("Expected IV got #{four}")
 
@@ -35,3 +40,15 @@ IO.puts("Expected V got #{five}")
 
 eight = RomanNumerals.intToRoman(8)
 IO.puts("Expected VIII got #{eight}")
+
+nine = RomanNumerals.intToRoman(9)
+IO.puts("Expected IX got #{nine}")
+
+ten = RomanNumerals.intToRoman(10)
+IO.puts("Expected X got #{ten}")
+
+twenty_seven = RomanNumerals.intToRoman(27)
+IO.puts("Expected XXVII got #{twenty_seven}")
+
+thirty_nine = RomanNumerals.intToRoman(39)
+IO.puts("Expected XXXIX got #{thirty_nine}")
